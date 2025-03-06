3dbdb0d4e7d678c0d7dc1643b91cba359d98f57c
ThatoSeluku <Thato1000Seluku@gmail.com>
2025-03-06 16:53:21 +0200

    Added search and filter capability

---
+++ Search_And_Filter_Galleries.js
@@ -0,0 +1,8 @@
+//Search with a text field search bar:
+    //Specifically here this is for anything that starts with what's in the search box":
+Filter('Invoice Data', StartsWith(Title, Searchbox_Text.Text))
+
+//Search functionality basically 
+Search('Invoice Data', Txt_Search.Text,Title)
+
+
